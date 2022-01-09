257a230e155a19e02a41ceed160eb79a5ffac4a3
Patrick Grossmann <patrickdgro@gmail.com>
2022-01-09 18:01:31 +0100

    feat: updated onedark colorscheme

--- lua/plugins/config/onedark.lua
+++ lua/plugins/config/onedark.lua
@@ -1,9 +1,6 @@
--- remove default keymap for toggle style
-vim.g.onedark_toggle_style_keymap = "<nop>"
-
 -- setup onedark
-require("onedark").setup()
+require("onedark").setup{
+	toggle_style_key = "<nop>",
+}
 
--- change colorscheme to trigger autocmdgroup colorscheme
--- such as in nvim-web-devicons
-vim.cmd "colorscheme onedark"
+require("onedark").load()
